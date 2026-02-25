0285f1af84a72ea8cb721acc4e22fff3f49f056e
Whyij <207023168@qq.com>
2026-02-26 02:44:32 +0800

    1

--- src/main/java/com/whyij/neu/NotEnoughUpgradeDataGenerator.java
+++ src/main/java/com/whyij/neu/NotEnoughUpgradeDataGenerator.java
@@ -1,11 +1,18 @@
 package com.whyij.neu;
 
+import com.whyij.neu.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class NotEnoughUpgradeDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(ModEnUsLangProvider::new);
+		pack.addProvider(ModZhCnLangProvider::new);
+		pack.addProvider(ModModelsProvider::new);
+		pack.addProvider(ModRecipesProvider::new);
+		pack.addProvider(ModItemTagsProvider::new);
 	}
 }
