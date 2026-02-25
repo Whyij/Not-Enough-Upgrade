7c4e4f43d1d1628f43534cbadee5d7ed472f0c84
Whyij <207023168@qq.com>
2026-02-26 00:52:50 +0800

    stp

--- src/main/java/com/whyij/neu/NotEnoughUpgrade.java
+++ src/main/java/com/whyij/neu/NotEnoughUpgrade.java
@@ -1,5 +1,6 @@
 package com.whyij.neu;
 
+import com.whyij.neu.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -19,6 +20,7 @@ public class NotEnoughUpgrade implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
+		ModItems.registerModItems();
 		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/com/whyij/neu/item/ModItems.java
+++ src/main/java/com/whyij/neu/item/ModItems.java
@@ -1,4 +1,39 @@
 package com.whyij.neu.item;
 
+import com.whyij.neu.NotEnoughUpgrade;
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.*;
+
+import java.util.function.Function;
+
 public class ModItems {
+    public static final Item STONE_UPGRADE_SMITHING_TEMPLATE = registerItem("stone_upgrade_smithing_template", SmithingTemplateItem::createNetheriteUpgradeTemplate, (new Item.Properties()).rarity(Rarity.COMMON));
+
+    public static Item registerItem(String string, Function<Item.Properties, Item> function, Item.Properties properties) {
+        ResourceKey<Item> resourceKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(NotEnoughUpgrade.MOD_ID,string));
+        Item item = (Item)function.apply(properties.setId(resourceKey));
+        if (item instanceof BlockItem blockItem) {
+            blockItem.registerBlocks(Item.BY_BLOCK, item);
+        }
+
+        return (Item)Registry.register(BuiltInRegistries.ITEM, resourceKey, item);
+    }
+
+
+    private static void addItemToIG(FabricItemGroupEntries fabricItemGroupEntries){
+        fabricItemGroupEntries.addBefore(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE,STONE_UPGRADE_SMITHING_TEMPLATE);
+    }
+
+    public static void registerModItems(){
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.INGREDIENTS).register(ModItems::addItemToIG);
+        NotEnoughUpgrade.LOGGER.info("Registering Items");
+    }
+
 }
